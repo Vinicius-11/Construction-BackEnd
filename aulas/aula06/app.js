12ba83a3711d396729b08900562d6f48a9eae3c3
Vinicius <viniciusdub8@gmail.com>
2025-09-16 21:47:41 -0300

    Adicionando arquivos da Aula06, Criando testes

--- aulas/aula06/app.js
+++ aulas/aula06/app.js
@@ -1,17 +1,41 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
+const indexRouter = require("./routes/index");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
-module.exports = app;
+const tarefas = [];
+
+app.get("/tarefas", (req, res) => {
+  res.json();
+});
+
+app.post("/tarefas", (req, res) => {
+  const novaTarefa = { 
+    ...req.body, 
+    id: tarefas.length + 1 
+};
+    tarefas.push(novaTarefa);
+  res.status(201).json(novaTarefa);
+});
+
+app.get("/tarefas/:id", (req, res) => {
+  const { id } = req.params;
+  const tarefaEncontrada = tarefas.find((item) => 
+    item.id === parseInt(id));
+  if (tarefaEncontrada) 
+    return res.json(tarefaEncontrada);
+  res.status(404).json({msg: "Tarefa não encontrada"})
+  res.json(tarefaEncontrada)
+})
+
+module.exports = app; 
