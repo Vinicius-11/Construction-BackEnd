8bb0ee632b91b015cc12143d725a6394bc09bcbc
Vinicius <viniciusdu8@gmail.com>
2025-09-23 21:50:02 -0300

    Adicionando arquivos da aula06, Controller e Model

--- aulas/aula06/app.js
+++ aulas/aula06/app.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
 // importando TAREFAS.JS para o APP
-const tarefaRouter = require("./routes/tarefas");
+const tarefaRouter = require("./routes/tarefaRouter");
 
 const app = express();
 
