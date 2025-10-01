4ec87285694bf16689157eebdbe3b57feabd5931
Vinicius <viniciusdu8@gmail.com>
2025-09-30 21:45:31 -0300

    Adicionando códigos da prática 5

---
+++ praticas/pratica05/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// Rotas
+const indexRouter = require("./routes/index");
+const tarefaRouter = require("./routes/tarefaRouter");
+
+app.use("/", indexRouter);
+app.use("/tarefas", tarefaRouter);
+app.use("/produtos", tarefaRouter);
+
+module.exports = app;
